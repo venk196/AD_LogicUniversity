f4e48b8c100c53c069f526d0f9b697f5d2a16e7e
Venkatachalam Krishnamoorthy Sivakamy <venkatachalamks.96@gmail.com>
2019-08-16 16:24:45 +0800

    Siqi Changes with Session Id

--- AD_CF/Controllers/ReportController.cs
+++ AD_CF/Controllers/ReportController.cs
@@ -12,6 +12,7 @@ namespace AD_CF.Controllers
 {
     public class ReportController : Controller
     {
+        UserServices userServices = new UserServices();
         // GET
         public ActionResult Trend()
         {
@@ -20,69 +21,92 @@ namespace AD_CF.Controllers
 
         public ActionResult Others2(string sessionId)
         {
-            using (var db = new InventoryDbContext())
+            if (sessionId != null && userServices.getUserCountBySessionId(sessionId) == true)
             {
-                List<Requisition> req = new List<Requisition>();
-                req = db.requisitions.ToList();
-                List<Requisition> requ = new List<Requisition>();
-                List<Department> dp = new List<Department>();
-                dp = db.departments.ToList();
-                ProductCatalogue pc = new ProductCatalogue();
-                List<double> total = new List<double>();
-                List<string> department = new List<string>();
-                ProductSupplier ps = new ProductSupplier();
-                double totalprice = 0;
-                foreach (var dps in dp)
+
+                using (var db = new InventoryDbContext())
                 {
-                    foreach (var reqs in req)
+                    List<Requisition> req = new List<Requisition>();
+                    req = db.requisitions.ToList();
+                    List<Requisition> requ = new List<Requisition>();
+                    List<Department> dp = new List<Department>();
+                    dp = db.departments.ToList();
+                    ProductCatalogue pc = new ProductCatalogue();
+                    List<double> total = new List<double>();
+                    List<string> department = new List<string>();
+                    ProductSupplier ps = new ProductSupplier();
+                    double totalprice = 0;
+                    foreach (var dps in dp)
                     {
-                        if (dps.id == reqs.department)
+                        foreach (var reqs in req)
                         {
-                            if (reqs.status == "approved" || reqs.status == "partial")
+                            if (dps.id == reqs.department)
                             {
-                                if (reqs.datecompleted != null && reqs.datecompleted.Value.Month == DateTime.Now.AddMonths(-1).Month && reqs.datecompleted.Value.Year == DateTime.Now.Year)
+                                if (reqs.status == "approved" || reqs.status == "partial")
                                 {
-                                    requ.Add(reqs);
+                                    if (reqs.datecompleted != null && reqs.datecompleted.Value.Month == DateTime.Now.AddMonths(-1).Month && reqs.datecompleted.Value.Year == DateTime.Now.Year)
+                                    {
+                                        requ.Add(reqs);
+                                    }
                                 }
                             }
                         }
-                    }
-                    foreach (var requs in requ)
-                    {
-                        foreach (var productReq in requs.productReqs)
+                        foreach (var requs in requ)
                         {
-                            pc = db.productCatalogues.Where(x => x.itemNumber == productReq.productitemnumber).FirstOrDefault();
-                            ps = db.productSuppliers.Where(x => x.itemNumber == productReq.productitemnumber && x.preference == 1).FirstOrDefault();
-                            totalprice = totalprice + pc.quantity * ps.price;//I can't find the supplier in Requisition and ProductReq model, so i can't get the price, just add one temperory¡£
+                            foreach (var productReq in requs.productReqs)
+                            {
+                                pc = db.productCatalogues.Where(x => x.itemNumber == productReq.productitemnumber).FirstOrDefault();
+                                ps = db.productSuppliers.Where(x => x.itemNumber == productReq.productitemnumber && x.preference == 1).FirstOrDefault();
+                                totalprice = totalprice + pc.quantity * ps.price;//I can't find the supplier in Requisition and ProductReq model, so i can't get the price, just add one temperory¡£
+                            }
                         }
+                        total.Add(totalprice);
+                        department.Add(dps.departmentName);
                     }
-                    total.Add(totalprice);
-                    department.Add(dps.departmentName);
+                    ViewData["total"] = total;
+                    ViewData["department"] = department; User user = db.users.Where(x => x.sessionId == sessionId).First();
+                    ViewData["staffname"] = user.employee.empName;
+                    ViewData["sessionId"] = sessionId;
+
+                    return View();
                 }
-                ViewData["total"] = total;
-                ViewData["department"] = department;
-                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Login");
             }
         }
 
         public ActionResult Others(string sessionId)
         {
-            using (var db = new InventoryDbContext())
+            if (sessionId != null && userServices.getUserCountBySessionId(sessionId) == true)
             {
-                List<Requisition> rt = new List<Requisition>();
-                ItemQuantity iq = new ItemQuantity();
-                rt = db.requisitions.ToList();
-                foreach (var rts in rt)
-                {
-                    foreach (var proReqs in rts.productReqs)
-                    {
-                        iq.add(proReqs.productitemnumber, proReqs.productDesc, proReqs.unitOfMeasure, proReqs.deliveredQuantity);
-                    }
 
+                using (var db = new InventoryDbContext())
+                {
+                    List<Requisition> rt = new List<Requisition>();
+                    ItemQuantity iq = new ItemQuantity();
+                    rt = db.requisitions.ToList();
+                    foreach (var rts in rt)
+                    {
+                        foreach (var proReqs in rts.productReqs)
+                        {
+                            iq.add(proReqs.productitemnumber, proReqs.productDesc, proReqs.unitOfMeasure, proReqs.deliveredQuantity);
+                        }
+
+                    }
+                    User user = db.users.Where(x => x.sessionId == sessionId).First();
+                    ViewData["staffname"] = user.employee.empName;
+                    ViewData["sessionId"] = sessionId;
+
+                    return View(iq);
                 }
-                return View(iq);
             }
 
+            else
+            {
+                return RedirectToAction("Login", "Login");
+            }
         }
 
     }
